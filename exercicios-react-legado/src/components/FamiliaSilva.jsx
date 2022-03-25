297a22af1c9eeda55bb29a0c53aa1f7f8ac731cd
DaniloIS <dailosantos-pg@hotmail.com>
2022-03-25 14:53:48 -0300

    Relação entre componentes

---
+++ exercicios-react-legado/src/components/FamiliaSilva.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Membro from "./Membro";
+
+export default props => 
+    <div>
+        <Membro name='Danilo' lastName='Silva' />
+        <Membro name='Diego' lastName='Silva' />
+        <Membro name='Pedro' lastName='Silva' />
+        <Membro name='Fernanda' lastName='Silva' />
+    </div>
--- exercicios-react-legado/src/index.js
+++ exercicios-react-legado/src/index.js
@@ -5,12 +5,20 @@ import ReactDOM from 'react-dom';
 //import Primeiro from './components/PrimeiroComponente'
 import { CompA, CompB } from './components/DoisComponente'
 import Mult from './components/MultElementos'
+import FamiliaSilva from './components/FamiliaSilva'
+import Familia from './components/Familia'
+import Membro from './components/Membro'
 
 ReactDOM.render(
     <div>
-        <CompA value='Olá eu sou A' />
+        {/*<CompA value='Olá eu sou A' />
         <CompB value='Olá eu sou B' />
         <Mult />
+        <FamiliaSilva />*/}
+        <Familia>
+            <Membro name='Danilo' lastName='Isidoro' />
+            <Membro name='Diego' lastName='Isidoro' />
+        </Familia>
     </div>,
     document.getElementById('root')
 )
