9e1ea2858642c910179725c33b53a96cf5eacd0c
DaniloIS <dailosantos-pg@hotmail.com>
2022-03-25 14:37:52 -0300

    Retornar Múltiplos Elementos

--- exercicios-react-legado/src/index.js
+++ exercicios-react-legado/src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom';
 //import { Primeiro } from './components/PrimeiroComponente'
 //import Primeiro from './components/PrimeiroComponente'
 import { CompA, CompB } from './components/DoisComponente'
+import Mult from './components/MultElementos'
 
 ReactDOM.render(
     <div>
         <CompA value='Olá eu sou A' />
         <CompB value='Olá eu sou B' />
+        <Mult />
     </div>,
     document.getElementById('root')
 )
